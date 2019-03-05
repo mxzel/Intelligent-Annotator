93d3adaeb3b2f3699b42dc8ac0ded68f0cccef75
zmx976508106 <976508106@qq.com>
2019-03-05 18:00:44 +0800

    删除部分注释

--- annotator/models.py
+++ annotator/models.py
@@ -2,68 +2,41 @@ from django.db import models
 from django.contrib.postgres.fields import *
 
 
-class ProjectInfo(models.Model):
-    """
-    project_id = "project_id"
-    project_name = "project_name"
-    project_tags = "project_tags"
-    """
+class Project(models.Model):
+    """创建的项目信息"""
     project_id = models.AutoField(primary_key=True)
     project_name = models.CharField(max_length=20, unique=True)
     project_tags = ArrayField(base_field=models.CharField(max_length=20, unique=True, default=None), default=list)
-    # project_process = models.FloatField(default=0.0)
     sentence_labeled = models.IntegerField(default=0)
     sentence_unlabeled = models.IntegerField(default=0)
 
 
-
-class FileInfo(models.Model):
-    """
-    file_id = "file_id"
-    file_name = "file_name"
-    project_id = "project_id"
-    """
+class File(models.Model):
+    """上传的文件信息"""
     file_id = models.AutoField(primary_key=True)
     file_name = models.CharField(max_length=20, unique=False)
-    project_id = models.ForeignKey(ProjectInfo, on_delete=models.CASCADE)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
 
 
 class BaseTags(models.Model):
+    """基础标签？"""
     tag_name = models.CharField(max_length=20, unique=True)
 
 
 class UnLabeledData(models.Model):
-    """
-    unlabeled_id = "unlabeled_id"
-    file_id = "file_id"
-    data_content = "data_content"
-    upload_time = "upload_time"
-    """
+    """未标注数据"""
     unlabeled_id = models.AutoField(primary_key=True)
-    file_id = models.ForeignKey(FileInfo, on_delete=models.CASCADE)
-    project_id = models.ForeignKey(ProjectInfo, on_delete=models.CASCADE)
+    file_id = models.ForeignKey(File, on_delete=models.CASCADE)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
     data_content = models.CharField(max_length=100, unique=False)
     upload_time = models.DateTimeField()
 
 
 class LabeledData(models.Model):
-    """
-    labeled_id = "labeled_id"
-    file_id = "file_id"
-    data_content = "data_content"
-    labeled_time = "labeled_time"
-    labeled_content = "labeled_content"
-    predicted_relation = "predicted_relation"
-    predicted_e1 = "predicted_e1"
-    predicted_e2 = "predicted_e2"
-    labeled_relation = "labeled_relation"
-    labeled_e1 = "labeled_e1"
-    labeled_e2 = "labeled_e2"
-    additional_info = "additional_info"
-    """
+    """已标注数据"""
     labeled_id = models.AutoField(primary_key=True)
-    file_id = models.ForeignKey(FileInfo, on_delete=models.CASCADE)
-    project_id = models.ForeignKey(ProjectInfo, on_delete=models.CASCADE)
+    file_id = models.ForeignKey(File, on_delete=models.CASCADE)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
     data_content = models.CharField(max_length=100, unique=False)
     labeled_time = models.DateTimeField()
     labeled_content = models.CharField(max_length=120, unique=False)

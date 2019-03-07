e904d8799849083880364640cc47c739122362cc
zmx976508106 <976508106@qq.com>
2019-03-07 10:05:54 +0800

    中间事务层重构

--- annotator/tests.py
+++ annotator/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
-
+from django.http import JsonResponse
 # Create your tests here.
+
+def test_connect(request):
+    return JsonResponse(data={"training_process": 50, "code": 200, "message": "annotate success"})

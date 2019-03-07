d75f1957daf9b446f9a8c2e11194bceb68aa5aed
zmx976508106 <976508106@qq.com>
2019-03-07 10:06:23 +0800

    允许外网访问

--- IntelligentAnnotator/settings.py
+++ IntelligentAnnotator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0r-c^g)_$yt7ob7xmd_ya6=q3jgn-8kmvcsj8ae*hb)hgdy3*k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
